8a7bd4b94497f2f84e8530c341dcf02ae66ee5f7
Simon Steshin <simon.steshin@gmail.com>
2024-11-13 13:11:01 +0000

    Improve performance of PAINS calculation

--- notebooks/filter_pains.py
+++ notebooks/filter_pains.py
@@ -2,8 +2,19 @@ from loguru import logger
 import medchem as mc
 import pandas as pd
 import argparse
+import multiprocessing as mp
+from tqdm import tqdm
+from functools import partial
+import datamol as dm
 
 from pathlib import Path
+
+
+def chunks(lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
+
 
 def filter_pains(data: pd.DataFrame, column_name: str = "smiles") -> pd.DataFrame:
     """Filters out PAINS alerts. Works REALLY long time. ~10 hours for BindingDB.
@@ -17,13 +28,31 @@ def filter_pains(data: pd.DataFrame, column_name: str = "smiles") -> pd.DataFram
     """
     data = data.copy()
     logger.info("Molecules before the PAINS elimination: {}", len(data))
-    is_good = mc.functional.alert_filter(
-        mols=data[column_name].tolist(),
+    logger.info("Converting molecules into datamol format...")
+    with mp.Pool(mp.cpu_count()) as pool:
+        molecules = list(tqdm(pool.imap(dm.to_mol, data[column_name]), total=len(data[column_name])))
+    logger.info("Done.")
+    logger.info("Filtering PAINS...")
+
+    BATCH_SIZE = 1000
+    batches_of_molecules = list(chunks(molecules, BATCH_SIZE))
+    
+    # Filter PAINS function
+    f = partial(
+        mc.functional.alert_filter,
         alerts=["PAINS"],
-        n_jobs=-1,
-        progress=True,
-        return_idx=False,
+        n_jobs=0,
+        progress=False,
+        return_idx=False
     )
+
+    num_processes = min(len(batches_of_molecules), mp.cpu_count())
+    is_good = []
+    with mp.Pool(processes=num_processes) as pool:
+        result_iter = pool.imap(f, batches_of_molecules)
+        for result in tqdm(result_iter, total=len(batches_of_molecules), desc="Processing batches"):
+            is_good.extend(result)
+
     data = data[is_good]
     logger.info("Molecules after the PAINS elimination: {}", len(data))
     return data
